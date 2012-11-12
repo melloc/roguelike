6bca25de81ecc3021419d396749accf168d923d8
Cody Mello <cody_mello@brown.edu>
2012-11-12 06:39:12 -0500

    Created a Lanterna frontend to serve as a framework for our roguelike.
    Lanterna provides methods for drawing to a screen, but little else. As a
    result, it is necessary to wrap it in something generic that can be used
    for developing a game. LanternaFrontend and Application serve this
    goals.
    
    LanternaFrontend is meant to be somewhat more generic than Application,
    and just assumes that some type of drawing to the console with key
    presses is happening. Application makes several more assumptions about
    how the application itself behaves and functions.
    
    This commit includes an interactive test for checking the behaviour of
    the several new files. I plan to add these tests to a new suite
    called "interactive" at some point, so that they will not run
    automatically and must be run under a separate Maven profile. The test
    will also need to be modified so that it can actually end, instead of
    requiring ^C.
    
    GameAction is meant to represent an action that occurs inside the game.
    Please read its Javadoc and the link provided there for more information
    on how it is intended to be used.

---
+++ engine/src/main/java/edu/brown/cs/roguelike/engine/graphics/Application.java
@@ -0,0 +1,73 @@
+package edu.brown.cs.roguelike.engine.graphics;
+
+import java.util.Stack;
+
+import com.googlecode.lanterna.input.Key;
+import com.googlecode.lanterna.screen.Screen;
+
+import cs195n.Vec2i;
+
+/**
+ * Represents an application that uses Lanterna. Provides the framework for
+ * building a console game.
+ */
+public abstract class Application extends LanternaFrontend {
+
+    /**
+     * This stack contains all elements in the game that should be drawn.
+     * The element on top of the stack is considered the `active` element,
+     * and receives all key presses.
+     */
+	protected Stack<Layer> layers = new Stack<Layer>();
+	
+	public Application(String title) {
+		super(title);
+	}
+
+	@Override
+	protected void onTick(long nanosSincePreviousTick) {
+		for (Layer layer : layers)
+			layer.tick(nanosSincePreviousTick);
+	}
+
+	@Override
+	protected void onDraw(Screen s) {
+		for (Layer layer : layers)
+			layer.doDraw(s);
+	}
+	
+    /**
+     * Takes care of any initialization that is necessary for the game. For 
+     * example, any {@link Layer}'s that need to be added to the stack should
+     * be added here. Any other preparation that is necessary should also be 
+     * done here.
+     *
+     * @param screenSize The initial size of the screen. This is provided in 
+     * case there is a need to determine where things should initially be drawn
+     * until the next resize (which may never happen, so passing this in is 
+     * sort of important).
+     *
+     * @return Boolean indicating whether initialization was sucessful.
+     */
+	protected abstract boolean initialize(Vec2i screenSize);
+
+	@Override
+	protected void onKeyPressed(Key k) {
+		if (!layers.isEmpty()) {
+			Layer active = layers.peek();
+			active.propagateAction(active.getActionForKey(k));
+		}
+	}
+	
+	private boolean initialized = false;
+
+	@Override
+	protected void onResize(Vec2i newSize) {
+		if (!initialized)
+			initialized = initialize(newSize);
+			
+		for (Layer layer : layers)
+			layer.updateSize(newSize);
+	}
+
+}
--- engine/src/main/java/edu/brown/cs/roguelike/engine/graphics/LanternaFrontend.java
+++ engine/src/main/java/edu/brown/cs/roguelike/engine/graphics/LanternaFrontend.java
@@ -1,7 +1,141 @@
 package edu.brown.cs.roguelike.engine.graphics;
 
-public class LanternaFrontend {
+import java.util.Date;
+
+import cs195n.Vec2i;
+import com.googlecode.lanterna.TerminalFacade;
+import com.googlecode.lanterna.screen.Screen;
+import com.googlecode.lanterna.terminal.TerminalSize;
+import com.googlecode.lanterna.input.Key;
+
+/**
+ * This class provides a framework in which we can develop an application that
+ * uses the Lanterna library for drawing on the terminal.
+ *
+ * @see <a href="http://code.google.com/p/lanterna/">Lanterna's Site</a>
+ */
+public abstract class LanternaFrontend {
+	
+    public LanternaFrontend(String title) {
 
 
+    }
+
+    /**
+	 * Use to update any state that changes over time. The amount of time that has
+     * passed depends on the length of time that the event loop takes to complete.
+	 * 
+	 * @param nanosSincePreviousTick	approximate number of nanoseconds since the previous call
+	 *                              	to onTick
+	 */
+	protected abstract void onTick(long nanosSincePreviousTick);
+	
+	/**
+	 * This function is called every time the screen needs to be updated.
+     *
+	 * @param s		A {@link Screen} object used for writing characters to the terminal.
+	 */
+	protected abstract void onDraw(Screen s);
+	
+	/**
+     * This method is called with a Lanterna {@link Key} event each time 
+     * a key is pressed. Ideally, the key is then turned into a Lanterna
+     * agnostic representation that is then passed throughout the game.
+     *
+	 * @param e		A Lanterna {@link Key} representing the input event.
+     *
+     * @see {@link edu.brown.cs.roguelike.engine.events.GameAction}
+	 */
+	protected abstract void onKeyPressed(Key k);
+	
+	/**
+	 * Called when the size of the drawing area changes. Any subsequent calls to onDraw should note
+	 * the new size and be sure to fill the entire area appropriately. Guaranteed to be called
+	 * before the first call to onDraw.
+	 * 
+	 * @param newSize	the new size of the drawing area.
+	 */
+	protected abstract void onResize(Vec2i newSize);
+
+    /**
+     * Used to indicate if the game should be running right now. Controls the
+     * event loop, too. When this is set to false, the loop will end, and any
+     * necessary cleanup should be performed.
+     */
+	private boolean running = false;
+	
+    /**
+	 * Begin processing events. None of the <code>on</code>*** methods will be called before this is
+	 * called.
+	 */
+	public final void startup() {
+		if (!running) {
+			running = true;
+			doStartup();
+		}
+	}
+
+	/**
+	 * Stop processing events. This causes the event loop to stop once it makes 
+     * it through the current loop. Because of this, some events may still get 
+     * called after this method is called.
+	 * <br><br>
+	 * Calling this method is not strictly necessary, but useful if you want to close the front-end
+	 * without exiting the program (e.g. if you write a map editor that directly launches the game).
+	 */
+	public final void shutdown() {
+		if (running) {
+			running = false;
+		}
+	}
+
+    /**
+     * Contains the main event loop that drives the game. This method will not 
+     * return until {@link LanternaFrontend#shutdown} is called.
+     */
+    final void doStartup() {
+        Screen screen = TerminalFacade.createScreen();
+        screen.startScreen();
+        {
+            TerminalSize size = screen.getTerminalSize();
+            onResize(new Vec2i(size.getColumns(),size.getRows()));
+        }
+
+        long time = new Date().getTime();
+        while (running) {
+            // First do anything that we need to do for updating screen size.
+            if (screen.resizePending()) {
+                screen.refresh();
+                TerminalSize size = screen.getTerminalSize();
+                onResize(new Vec2i(size.getColumns(),size.getRows()));
+            }
+
+            // First do anything that we need to do for updating screen size.
+            // Then we draw the screen.
+            screen.clear();
+            onDraw(screen);
+            screen.refresh();
+            
+            // We can now give the program any key presses
+            Key key = screen.readInput();
+            if (key != null)
+                onKeyPressed(key);
+
+            // Then we take care of ticks.
+            long currentTime = new Date().getTime();
+            onTick(currentTime - time);
+            time = currentTime;
+        }
+        screen.stopScreen();
+    }
+    
+    /**
+     * This method turns on debug mode, which is useful for development.
+     */
+    final void doSetDebugMode() {
+        // Nothing is done here currently.
+        // If logging is added at some point (such as log4j), then this should
+        // set the logging level to something proper.
+    }
 
 }
