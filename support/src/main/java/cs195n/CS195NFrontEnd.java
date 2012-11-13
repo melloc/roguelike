46b98b949d487bf9570ab9d99a3bd1b57258c920
Liam Elberty <lelberty@cs.brown.edu>
2012-11-13 11:05:08 -0500

    Fixes for broken code on pull

--- support/src/main/java/cs195n/CS195NFrontEnd.java
+++ support/src/main/java/cs195n/CS195NFrontEnd.java
@@ -247,7 +247,7 @@ public abstract class CS195NFrontEnd {
 	 * The default window size; should be passed to the constructor if the user does not specify a
 	 * window size.
 	 */
-	static final Vec2i DEFAULT_WINDOW_SIZE = new Vec2i(960, 540);
+	protected static final Vec2i DEFAULT_WINDOW_SIZE = new Vec2i(960, 540);
 	
 	/**
 	 * The minimum window size. Games are expected to work with resolutions at least this small;
