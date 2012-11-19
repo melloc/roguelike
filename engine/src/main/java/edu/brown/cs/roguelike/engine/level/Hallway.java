0964818cfe82a346fc284cee2a08a24442fce92c
Jake Ellis <jte@cs.brown.edu>
2012-11-18 19:20:43 -0500

    Renamings

--- engine/src/main/java/edu/brown/cs/roguelike/engine/level/Hallway.java
+++ engine/src/main/java/edu/brown/cs/roguelike/engine/level/Hallway.java
@@ -1,9 +1,5 @@
 package edu.brown.cs.roguelike.engine.level;
 
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
